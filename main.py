0a8d17eba1ba1192fc7e1e48b25bc6b0ef23c9f5
DongWon Lee <144747366+DongWonLee2@users.noreply.github.com>
2024-07-26 02:10:10 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+from routes.route import router
+
+app = FastAPI()
+
+# include_router: 다른 모듈에서 정의된 라우터를 메인 애플리케이션에 포함. 코드 구조를 모듈화하고 유지보수에 용이.
+app.include_router(router)
