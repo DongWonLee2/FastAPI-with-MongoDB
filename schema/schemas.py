d8664821dfb1696e8f0942d85d847a0060e89f34
DongWon Lee <144747366+DongWonLee2@users.noreply.github.com>
2024-07-27 00:16:56 +0900

    Update schemas.py

--- schema/schemas.py
+++ schema/schemas.py
@@ -1,4 +1,4 @@
-def individual_serial(todo) -> dict: # MongoDB의 단일 문서를 직렬화하여 딕셔너리 형태로 반환한다.
+def individual_serial(todo) -> dict: # MongoDB의 단일 문서를 직렬화하여 딕셔너리 형태로 반환한다. 문서 지향 데이터베이스의 데이터를 Python 객체로 변환.
     return{
         "id": str(todo["_id"]),
         "name": todo["name"],
